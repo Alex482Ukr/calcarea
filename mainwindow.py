7b429f876d9265082e4a57a6d865c265ac841b63
Alex Linux <alex482ukr@gmail.com>
2024-08-03 11:00:12 +0300

    first alpha

---
+++ mainwindow.py
@@ -0,0 +1,108 @@
+# This Python file uses the following encoding: utf-8
+import sys
+from decimal import Decimal as Dec
+
+from PySide6.QtWidgets import QApplication, QMainWindow, QTableWidgetItem
+
+from PySide6.QtCore import Qt
+
+# Important:
+# You need to run the following command to generate the ui_form.py file
+#     pyside6-uic form.ui -o ui_form.py, or
+#     pyside2-uic form.ui -o ui_form.py
+from ui_form import Ui_MainWindow
+
+class MainWindow(QMainWindow):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        self.ui.pushButton.clicked.connect(self.new_row)
+        self.ui.tableWidget.itemChanged.connect(self.update_values)
+
+    def new_row(self):
+        self.ui.tableWidget.itemChanged.disconnect(self.update_values)
+
+        row = self.ui.tableWidget.rowCount()
+        self.ui.tableWidget.insertRow(row)
+
+        for col in range(6):
+            self.ui.tableWidget.setItem(row, col, QTableWidgetItem())
+            self.ui.tableWidget.item(row, col).setText('0.00')
+
+        self.ui.tableWidget.item(row, 0).setText('')
+
+        area, volume = self.ui.tableWidget.item(row, 4), self.ui.tableWidget.item(row, 5)
+        area.setFlags(area.flags() & ~Qt.ItemIsEditable)
+        volume.setFlags(volume.flags() & ~Qt.ItemIsEditable)
+
+        self.ui.tableWidget.itemChanged.connect(self.update_values)
+
+    def update_values(self):
+        self.ui.tableWidget.itemChanged.disconnect(self.update_values)
+
+        self.calculate_area()
+        self.calculate_volume()
+
+        self.sum_area()
+        self.sum_volume()
+
+        self.ui.tableWidget.itemChanged.connect(self.update_values)
+
+    def calculate_area(self):
+        for row in range(self.ui.tableWidget.rowCount()):
+            width, length = self.ui.tableWidget.item(row, 1), self.ui.tableWidget.item(row, 2)
+            width, length = width.text(), length.text()
+            width, length = width.replace(',', '.'), length.replace(',', '.')
+            width, length = Dec(width), Dec(length)
+
+            res = width * length
+            res = self.out_format(res, rounding=1)
+
+            self.ui.tableWidget.item(row, 4).setText(res)
+
+
+    def sum_area(self):
+        summ = Dec('0')
+        sum_a = Dec('0')
+
+        for row in range(self.ui.tableWidget.rowCount()):
+            summ += Dec(self.ui.tableWidget.item(row, 4).text())
+            letter = self.ui.tableWidget.item(row, 0).text()
+            if letter and letter[0] in ('A', 'a', 'А', 'а'):
+                sum_a += Dec(self.ui.tableWidget.item(row, 4).text())
+
+        self.ui.area_dwelling.setText(str(sum_a))
+        self.ui.area_economical.setText(str(summ - sum_a))
+        self.ui.area_total.setText(str(summ))
+
+    def calculate_volume(self):
+        for row in range(self.ui.tableWidget.rowCount()):
+            area, height = self.ui.tableWidget.item(row, 4), self.ui.tableWidget.item(row, 3)
+            area, height = area.text(), height.text()
+            area, height = area.replace(',', '.'), height.replace(',', '.')
+            area, height = Dec(area), Dec(height)
+
+            res = area * height
+            res = self.out_format(res, rounding=0)
+
+            self.ui.tableWidget.item(row, 5).setText(res)
+
+    def sum_volume(self):
+        summ = Dec('0')
+        for row in range(self.ui.tableWidget.rowCount()):
+            summ += Dec(self.ui.tableWidget.item(row, 5).text())
+        self.ui.volume_total.setText(str(summ))
+
+    def out_format(self, value, rounding=0):
+        value = round(value, rounding)
+        value = str(value)
+        return value
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    widget = MainWindow()
+    widget.show()
+    sys.exit(app.exec())
---
+++ ui_form.py
@@ -0,0 +1,171 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'form.ui'
+##
+## Created by: Qt User Interface Compiler version 6.7.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QAction, QBrush, QColor, QConicalGradient,
+    QCursor, QFont, QFontDatabase, QGradient,
+    QIcon, QImage, QKeySequence, QLinearGradient,
+    QPainter, QPalette, QPixmap, QRadialGradient,
+    QTransform)
+from PySide6.QtWidgets import (QApplication, QHeaderView, QLabel, QMainWindow,
+    QMenu, QMenuBar, QPushButton, QSizePolicy,
+    QStatusBar, QTableWidget, QTableWidgetItem, QTextBrowser,
+    QWidget)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(872, 394)
+        self.actionOpen = QAction(MainWindow)
+        self.actionOpen.setObjectName(u"actionOpen")
+        self.actionSave = QAction(MainWindow)
+        self.actionSave.setObjectName(u"actionSave")
+        self.action = QAction(MainWindow)
+        self.action.setObjectName(u"action")
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.tableWidget = QTableWidget(self.centralwidget)
+        if (self.tableWidget.columnCount() < 6):
+            self.tableWidget.setColumnCount(6)
+        __qtablewidgetitem = QTableWidgetItem()
+        self.tableWidget.setHorizontalHeaderItem(0, __qtablewidgetitem)
+        __qtablewidgetitem1 = QTableWidgetItem()
+        self.tableWidget.setHorizontalHeaderItem(1, __qtablewidgetitem1)
+        __qtablewidgetitem2 = QTableWidgetItem()
+        self.tableWidget.setHorizontalHeaderItem(2, __qtablewidgetitem2)
+        __qtablewidgetitem3 = QTableWidgetItem()
+        self.tableWidget.setHorizontalHeaderItem(3, __qtablewidgetitem3)
+        __qtablewidgetitem4 = QTableWidgetItem()
+        self.tableWidget.setHorizontalHeaderItem(4, __qtablewidgetitem4)
+        __qtablewidgetitem5 = QTableWidgetItem()
+        self.tableWidget.setHorizontalHeaderItem(5, __qtablewidgetitem5)
+        self.tableWidget.setObjectName(u"tableWidget")
+        self.tableWidget.setGeometry(QRect(10, 0, 641, 341))
+        self.pushButton = QPushButton(self.centralwidget)
+        self.pushButton.setObjectName(u"pushButton")
+        self.pushButton.setGeometry(QRect(660, 0, 201, 25))
+        self.area_dwelling = QTextBrowser(self.centralwidget)
+        self.area_dwelling.setObjectName(u"area_dwelling")
+        self.area_dwelling.setGeometry(QRect(770, 80, 91, 31))
+        self.label = QLabel(self.centralwidget)
+        self.label.setObjectName(u"label")
+        self.label.setGeometry(QRect(660, 80, 101, 31))
+        font = QFont()
+        font.setPointSize(12)
+        self.label.setFont(font)
+        self.label.setAlignment(Qt.AlignCenter)
+        self.label_3 = QLabel(self.centralwidget)
+        self.label_3.setObjectName(u"label_3")
+        self.label_3.setGeometry(QRect(660, 110, 101, 31))
+        self.label_3.setFont(font)
+        self.label_3.setAlignment(Qt.AlignCenter)
+        self.area_economical = QTextBrowser(self.centralwidget)
+        self.area_economical.setObjectName(u"area_economical")
+        self.area_economical.setGeometry(QRect(770, 110, 91, 31))
+        self.label_4 = QLabel(self.centralwidget)
+        self.label_4.setObjectName(u"label_4")
+        self.label_4.setGeometry(QRect(660, 140, 101, 31))
+        font1 = QFont()
+        font1.setPointSize(12)
+        font1.setBold(True)
+        self.label_4.setFont(font1)
+        self.label_4.setAlignment(Qt.AlignCenter)
+        self.area_total = QTextBrowser(self.centralwidget)
+        self.area_total.setObjectName(u"area_total")
+        self.area_total.setGeometry(QRect(770, 140, 91, 31))
+        self.volume_total = QTextBrowser(self.centralwidget)
+        self.volume_total.setObjectName(u"volume_total")
+        self.volume_total.setGeometry(QRect(770, 200, 91, 31))
+        self.label_6 = QLabel(self.centralwidget)
+        self.label_6.setObjectName(u"label_6")
+        self.label_6.setGeometry(QRect(660, 200, 101, 31))
+        self.label_6.setFont(font1)
+        self.label_6.setAlignment(Qt.AlignCenter)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QMenuBar(MainWindow)
+        self.menubar.setObjectName(u"menubar")
+        self.menubar.setGeometry(QRect(0, 0, 872, 22))
+        self.menuFile = QMenu(self.menubar)
+        self.menuFile.setObjectName(u"menuFile")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QStatusBar(MainWindow)
+        self.statusbar.setObjectName(u"statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.menubar.addAction(self.menuFile.menuAction())
+        self.menuFile.addAction(self.actionOpen)
+        self.menuFile.addAction(self.actionSave)
+        self.menuFile.addAction(self.action)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.actionOpen.setText(QCoreApplication.translate("MainWindow", u"Open", None))
+        self.actionSave.setText(QCoreApplication.translate("MainWindow", u"Save", None))
+        self.action.setText(QCoreApplication.translate("MainWindow", u"\u0417\u0431\u0435\u0440\u0435\u0433\u0442\u0438 \u044f\u043a", None))
+        ___qtablewidgetitem = self.tableWidget.horizontalHeaderItem(0)
+        ___qtablewidgetitem.setText(QCoreApplication.translate("MainWindow", u"\u0411\u0443\u043a\u0432\u0430", None));
+        ___qtablewidgetitem1 = self.tableWidget.horizontalHeaderItem(1)
+        ___qtablewidgetitem1.setText(QCoreApplication.translate("MainWindow", u"\u0428\u0438\u0440\u0438\u043d\u0430", None));
+        ___qtablewidgetitem2 = self.tableWidget.horizontalHeaderItem(2)
+        ___qtablewidgetitem2.setText(QCoreApplication.translate("MainWindow", u"\u0414\u043e\u0432\u0436\u0438\u043d\u0430", None));
+        ___qtablewidgetitem3 = self.tableWidget.horizontalHeaderItem(3)
+        ___qtablewidgetitem3.setText(QCoreApplication.translate("MainWindow", u"\u0412\u0438\u0441\u043e\u0442\u0430", None));
+        ___qtablewidgetitem4 = self.tableWidget.horizontalHeaderItem(4)
+        ___qtablewidgetitem4.setText(QCoreApplication.translate("MainWindow", u"\u041f\u043b\u043e\u0449\u0430", None));
+        ___qtablewidgetitem5 = self.tableWidget.horizontalHeaderItem(5)
+        ___qtablewidgetitem5.setText(QCoreApplication.translate("MainWindow", u"\u041e\u0431'\u0454\u043c", None));
+        self.pushButton.setText(QCoreApplication.translate("MainWindow", u"\u0414\u043e\u0434\u0430\u0442\u0438 \u0440\u044f\u0434\u043e\u043a", None))
+        self.area_dwelling.setHtml(QCoreApplication.translate("MainWindow", u"<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><meta charset=\"utf-8\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"hr { height: 1px; border-width: 0; }\n"
+"li.unchecked::marker { content: \"\\2610\"; }\n"
+"li.checked::marker { content: \"\\2612\"; }\n"
+"</style></head><body style=\" font-family:'Sans Serif'; font-size:9pt; font-weight:400; font-style:normal;\">\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">0</p></body></html>", None))
+        self.label.setText(QCoreApplication.translate("MainWindow", u"S\u0436\u0438\u0442\u043b\u043e\u0432\u0430", None))
+        self.label_3.setText(QCoreApplication.translate("MainWindow", u"S\u0433\u043e\u0441\u043f", None))
+        self.area_economical.setHtml(QCoreApplication.translate("MainWindow", u"<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><meta charset=\"utf-8\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"hr { height: 1px; border-width: 0; }\n"
+"li.unchecked::marker { content: \"\\2610\"; }\n"
+"li.checked::marker { content: \"\\2612\"; }\n"
+"</style></head><body style=\" font-family:'Sans Serif'; font-size:9pt; font-weight:400; font-style:normal;\">\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">0</p></body></html>", None))
+        self.label_4.setText(QCoreApplication.translate("MainWindow", u"S\u0437\u0430\u0433\u0430\u043b\u044c\u043d\u0430", None))
+        self.area_total.setHtml(QCoreApplication.translate("MainWindow", u"<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><meta charset=\"utf-8\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"hr { height: 1px; border-width: 0; }\n"
+"li.unchecked::marker { content: \"\\2610\"; }\n"
+"li.checked::marker { content: \"\\2612\"; }\n"
+"</style></head><body style=\" font-family:'Sans Serif'; font-size:9pt; font-weight:400; font-style:normal;\">\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">0</p></body></html>", None))
+        self.volume_total.setHtml(QCoreApplication.translate("MainWindow", u"<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><meta charset=\"utf-8\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"hr { height: 1px; border-width: 0; }\n"
+"li.unchecked::marker { content: \"\\2610\"; }\n"
+"li.checked::marker { content: \"\\2612\"; }\n"
+"</style></head><body style=\" font-family:'Sans Serif'; font-size:9pt; font-weight:400; font-style:normal;\">\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">0</p></body></html>", None))
+        self.label_6.setText(QCoreApplication.translate("MainWindow", u"V\u0437\u0430\u0433\u0430\u043b\u044c\u043d\u0438\u0439", None))
+        self.menuFile.setTitle(QCoreApplication.translate("MainWindow", u"\u0424\u0430\u0439\u043b", None))
+    # retranslateUi
+
